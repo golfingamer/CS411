8c5c60f20376474dcfdb3e01471f92ebfc123820
git <MDaigh@Michaels-MacBook-Pro.local>
2014-08-27 14:55:30 -0700

    Initial commit

---
+++ test/test/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  test
+//
+//  Created by Michael Daigh on 8/27/14.
+//  Copyright (c) 2014 Michael Daigh. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+	@autoreleasepool {
+	    
+	    // insert code here...
+	    NSLog(@"Hello, World!");
+	    
+	}
+    return 0;
+}
+
